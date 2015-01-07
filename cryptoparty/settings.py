fe0ff7da49bc4b8512eff34e8afa2e81683fec05
Víðir Valberg Guðmundsson <valberg@orn.li>
2015-01-07 21:16:28 +0100

    Settings and migrations.

--- cryptoparty/settings.py
+++ cryptoparty/settings.py
@@ -75,3 +75,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
