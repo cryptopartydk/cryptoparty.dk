0dbfda0634239d354d1209e7c6366ad7c6b4586a
Víðir Valberg Guðmundsson <valberg@orn.li>
2015-01-08 13:51:46 +0100

    Call to map tiles should also be https.

--- signup/static/js/main.js
+++ signup/static/js/main.js
@@ -2,7 +2,7 @@ var center = [55.679208653509434, 12.562839388847351];
 var prosa = [55.6790271772563, 12.562839388847351];
 var map = L.map('map').setView(center, 16);
 
-L.tileLayer('http://{s}.tiles.mapbox.com/v3/jchillerup.kmm460oi/{z}/{x}/{y}.png', {
+L.tileLayer('https://{s}.tiles.mapbox.com/v3/jchillerup.kmm460oi/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18
 }).addTo(map);
