d4565232bbebdfe7685153099231a73a3972fff6
Víðir Valberg Guðmundsson <valberg@orn.li>
2015-01-19 18:20:09 +0100

    Make admin a bit more readable.

--- signup/admin.py
+++ signup/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Attendee
 
-admin.site.register(Attendee)
+
+class AttendeeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'want_to_know', 'can_help_with')
+
+
+admin.site.register(Attendee, AttendeeAdmin)
