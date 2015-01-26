27c61df9ce8fff2244f542d9b12d7500e450162e
Víðir Valberg Guðmundsson <valberg@orn.li>
2015-01-26 12:17:23 +0100

    Temp site

--- cryptoparty/settings.py
+++ cryptoparty/settings.py
@@ -67,3 +67,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+TEMPLATE_DIRS = [
+    os.path.join(BASE_DIR, 'templates')
+]
