7a0f05f530f56144a9d1f33a6120201acf5a43b2
Víðir Valberg Guðmundsson <valberg@orn.li>
2015-04-27 23:34:15 +0200

    Removing memcached connection - it was slowing the uwsgi restarting process.

--- cryptoparty/settings.py
+++ cryptoparty/settings.py
@@ -113,10 +113,3 @@ EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = env('EMAIL_USE_TLS')
 DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
-
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '127.0.0.1:11211',
-    }
-}
